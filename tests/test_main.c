59bb7d16fb974d9bbb9cfa780adc6c3ad85c411f
surajgajavelly <saisurajgajavelly@gmail.com>
2025-10-23 15:49:05 +0000

    test: Add failing test for block splitting

--- tests/test_main.c
+++ tests/test_main.c
@@ -39,12 +39,27 @@ void test_free_should_reuse_memory(void)
     TEST_ASSERT_EQUAL_PTR(ptr1, ptr2);
 }
 
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+void test_malloc_should_split_large_block(void)
+{
+    void *ptr1 = my_malloc(100);
+    TEST_ASSERT_NOT_NULL(ptr1);
+
+    void *ptr2 = my_malloc(500);
+    TEST_ASSERT_NOT_NULL(ptr2);
+}
+
 int main(void) {
     UNITY_BEGIN(); // Sets up Unity
 
     // Run our tests
     RUN_TEST(test_malloc_basic_should_return_non_null);
     RUN_TEST(test_free_should_reuse_memory);
+    RUN_TEST(test_malloc_should_split_large_block);
 
     return UNITY_END(); // Reports the results
 }
